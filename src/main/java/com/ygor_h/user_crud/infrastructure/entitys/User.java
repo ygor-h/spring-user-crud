8fbf8a76e617ae05b8067f3c25ae79652ef174cc
ygor-h <ygorlh01@gmail.com>
2025-09-11 16:00:46 -0300

    feat: create User entity

---
+++ src/main/java/com/ygor_h/user_crud/infrastructure/entitys/User.java
@@ -0,0 +1,27 @@
+package com.ygor_h.user_crud.infrastructure.entitys;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "user")
+@Entity
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "cpf", unique = true, length = 11, nullable = false)
+    private String cpf;
+}
