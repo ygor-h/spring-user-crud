0f8c733cdc021e9a1cdecff1fb8069f23aa62c43
ygor-h <ygorlh01@gmail.com>
2025-09-11 15:26:53 -0300

    first-commit

---
+++ src/main/java/com/ygor_h/user_crud/UserCrudApplication.java
@@ -0,0 +1,13 @@
+package com.ygor_h.user_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ygor_h/user_crud/UserCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygor_h.user_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
